a05168fb0f0c51e91fe96d2f2428703452834750
pjkarlik <pjkarlik@hotmail.com>
2017-04-25 16:32:46 -0700

    on vacation playing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Render from './components/Forrest';
+import Render from './components/Branches';
 import { description, version } from '../version.json';
 require('../resources/styles/styles.css');
 
